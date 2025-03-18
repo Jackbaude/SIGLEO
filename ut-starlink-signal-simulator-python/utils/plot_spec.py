c581df70dfc82c173ac970ef9c0d73d5cd12a8d9
Jack <jack@Jacks-MacBook-Air-63.local>
2025-03-18 15:42:13 -0500

    Updating plot_spec.py to more closely match Matlab.

--- ut-starlink-signal-simulator-python/utils/plot_spec.py
+++ ut-starlink-signal-simulator-python/utils/plot_spec.py
@@ -12,87 +12,58 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import spectrogram, resample, windows
+from scipy.signal import resample, spectrogram
+from numpy.fft import fftshift
 
 def plot_spec(data, tstart, tdur, Fcr, Fsr, Fc, Fs, NFFT, Stitle=None):
     """
-    Plots a spectrogram of the provided signal.
+    Plots a spectrogram of the provided data.
 
     Parameters:
-    data : ndarray
-        Vector of complex samples.
-    tstart : float
-        Start time (relative to first sample).
-    tdur : float
-        Duration of data to process. If 0, plots entire signal.
-    Fcr : float
-        Center frequency of received signal.
-    Fsr : float
-        Sampling rate of provided signal.
-    Fc : float
-        Desired center frequency for the plot.
-    Fs : float
-        Desired sampling rate for spectrogram.
-    NFFT : int
-        Number of FFT points for the spectrogram.
-    Stitle : str, optional
-        Title for the spectrogram plot.
-
-    Returns:
-    fig : matplotlib.figure.Figure
-        The generated spectrogram figure.
+    data (np.array): Vector of complex samples.
+    tstart (float): Start of plot in seconds relative to beginning of input data.
+    tdur (float): Duration of plot in seconds. If 0, the whole section is plotted.
+    Fcr (float): Center frequency of Passband equivalent signal.
+    Fsr (float): Sampling rate of provided signal.
+    Fc (float): Center frequency desired to be plotted.
+    Fs (float): Desired plot sampling rate. Will resample provided signal to Fs if Fs != Fsr.
+    NFFT (int): Number of FFT points for spectrogram.
+    Stitle (str): Optional title for plot.
     """
-
-    # Default title
     if Stitle is None:
-        Stitle = f"Spectrogram centered at {Fc / 1e6} MHz"
+        Stitle = f"Spectrogram centered at {Fc}"
 
-    # If duration is 0, use the entire signal
     if tdur == 0:
         tdur = len(data) / Fsr
 
-    # Extract the required segment
-    seek_offset = int(np.floor(tstart * Fsr))
-    Nblock = int(np.floor(tdur * Fsr))
-    data = data[seek_offset : seek_offset + Nblock]
+    # Calculate the indices for the data segment
+    seekOffset = int(tstart * Fsr)
+    Nblock = int(tdur * Fsr)
+    data_segment = data[seekOffset:seekOffset + Nblock]
 
-    # Time vector
-    tyVec = np.arange(len(data)) / Fsr
+    # Time vector for the data segment
+    tyVec = np.arange(len(data_segment)) / Fsr
 
     # Resample if necessary
     if Fs != Fsr:
-        data = resample(data, int(len(data) * (Fs / Fsr)))
-        tyVec = np.linspace(0, tdur, len(data))
+        data_segment, tyVec = resample(data_segment, int(len(data_segment) * Fs / Fsr), t=tyVec)
 
-    # Shift frequency if needed
+    # Apply frequency shift if necessary
     if Fc != Fcr:
         Fshift = Fcr - Fc
-        data *= np.exp(1j * 2 * np.pi * Fshift * tyVec)
+        data_segment *= np.exp(1j * 2 * np.pi * Fshift * tyVec)
 
-    # Compute Spectrogram
-    fVec, tVec, Sxx = spectrogram(
-        data / np.sqrt(np.mean(np.abs(data) ** 2)),  # Normalize power
-        fs=Fs,
-        window=windows.kaiser(NFFT, 0.5),
-        nperseg=NFFT,
-        noverlap=NFFT // 2,
-        nfft=NFFT,
-        scaling="density",
-        mode="psd"
-    )
+    # Normalize data to have unit power
+    data_normalized = data_segment / np.sqrt(np.mean(np.abs(data_segment)**2))
 
-    # Convert to dB scale
-    Sxx_dB = 10 * np.log10(Sxx)
-    Sxx_dB[Sxx_dB < -90] = -90  # Thresholding at -90 dB
-
-    # Plot Spectrogram
-    fig, ax = plt.subplots(figsize=(10, 5))
-    c = ax.pcolormesh(tVec + tstart, fVec / 1e6, Sxx_dB, shading="auto", cmap="jet", vmin=-90)
-    plt.colorbar(c, label="Power Density (dB/Hz)")
-
-    ax.set_xlabel("Time (s)")
-    ax.set_ylabel("Frequency (MHz)")
-    ax.set_title(Stitle)
-
+    # Spectrogram calculation
+    f, t, Sxx = spectrogram(data_normalized, Fs, window='kaiser', nperseg=NFFT, noverlap=NFFT//2, nfft=NFFT, scaling='spectrum', mode='psd')
+    
+    # Plotting
+    plt.figure()
+    plt.pcolormesh(t, fftshift(f), fftshift(10 * np.log10(Sxx), axes=0), shading='gouraud', cmap='viridis')
+    plt.ylabel('Frequency (Hz)')
+    plt.xlabel('Time (s)')
+    plt.title(Stitle)
+    plt.colorbar(label='PSD (dB/Hz)')
     plt.show()
-    return fig
