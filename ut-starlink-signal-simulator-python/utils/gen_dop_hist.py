b6f3298b8f4d3dd9442866d015a775b11658bf02
Jack <jack@Jacks-MacBook-Air-63.local>
2025-03-19 16:32:42 -0500

    Adding to the file gen_dop_hist.py.

--- ut-starlink-signal-simulator-python/utils/gen_dop_hist.py
+++ ut-starlink-signal-simulator-python/utils/gen_dop_hist.py
@@ -1,2 +1,10 @@
+from scipy.constants import speed_of_light
+
 def gen_dop_hist(s):
     NMAX = 32000000
+
+    c = speed_of_light / 1e3  # Convert to km/s
+
+    lamda = c / s["fc"]
+
+    n_samples = s["fs"] * s["tdur"]
